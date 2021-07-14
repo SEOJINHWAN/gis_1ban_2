d034f27765b78cf4af2fea511774fb48ebcc83e5
SEO JINHWAN <sjhge08@gmail.com>
2021-07-14 10:25:43 +0900

    7/14 1ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,17 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import introduce
+from accountapp.views import introduce, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('introduce/', introduce, name='myself')
+    path('introduce/', introduce, name='introduce'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
